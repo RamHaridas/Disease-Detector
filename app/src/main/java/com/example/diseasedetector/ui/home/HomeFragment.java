d6a5eec8fd6e78e85a55b7226853888deda78ab0
Ram Haridas <ramharidas31@gmail.com>
2020-11-05 19:21:43 +0530

    Ram Haridas

--- app/src/main/java/com/example/diseasedetector/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/diseasedetector/ui/home/HomeFragment.java
@@ -12,16 +12,24 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.diseasedetector.R;
 
 public class HomeFragment extends Fragment {
 
-
+    LottieAnimationView covid;
     View root;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         root = inflater.inflate(R.layout.fragment_home, container, false);
+        init();
         return root;
+    }
+
+    private void init(){
+        covid = root.findViewById(R.id.covidAnim);
+        covid.setVisibility(View.VISIBLE);
+        covid.setAnimation(R.raw.covid_positive);
     }
 }
